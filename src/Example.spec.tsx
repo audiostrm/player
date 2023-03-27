58bc33baf7fc9ae8d9ebbd83bf4eda135919939d
callmenikk <formyauto75@gmail.com>
2023-03-27 19:41:25 +0400

    initial boilerplate

---
+++ src/Example.spec.tsx
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Example } from './Example';
+
+describe('[Component] Example', () => {
+  it('testing vitest tests', () => {
+    render(<Example />);
+
+    expect(screen.getByText('click 0')).toBeDefined();
+  });
+  
+  it('should call click on button and increment', () => {
+    render(<Example />);
+
+    fireEvent.click(screen.getByText('click 0'))
+
+    expect(screen.getByText('click 1')).toBeDefined();
+  });
+});
---
+++ src/Example.story.tsx
@@ -0,0 +1,13 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Example } from './Example';
+
+export default {
+  title: 'Example',
+  component: Example
+} as ComponentMeta<typeof Example>;
+
+const Template: ComponentStory<typeof Example> = () => (
+  <Example />
+);
+
+export const Primary: ComponentStory<typeof Example> = Template.bind({});
