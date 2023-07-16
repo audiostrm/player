b4faee1d6c8f3efef99b5fd31b9d1031088d24f2
formyauto75@gmail.com <formyauto75@gmail.com>
2023-07-16 19:40:28 +0400

    fix: build error

--- src/player.tsx
+++ src/player.tsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import './styles/index.css';
 import { Tracker } from './core/tracker';
 import { View } from './view';
-import { usePlayer } from 'dist';
 import { AudioContext } from './context/audio-context';
 
 export const Player = () => {
--- src/view/mobile/mobile-canvas.tsx
+++ src/view/mobile/mobile-canvas.tsx
@@ -3,7 +3,6 @@ import { Image } from '../desktop/components/audio-info';
 import { PlayerButton } from '@/core/controller/components/player-button';
 
 export const MobileCanvas = () => (
-  
   <div className="mobile-canvas">
     <Image />
     <PlayerButton />
