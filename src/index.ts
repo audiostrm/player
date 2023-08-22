cb7528d92efa0a2431fbba2404c062572f9649b0
formyauto75@gmail.com <formyauto75@gmail.com>
2023-08-22 23:31:07 +0400

    fix: idk issue

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { Player } from './player';
 export { PlayerProvider } from './provider';
 export { usePlayer } from './hooks/usePlayer';
 export { ScrobblerProvider } from './provider/scrobbler';
-export type { ScrobbleType } from './types/scrobble.types';
+export type { ScrobbleType } from './types/scrobble.types'
