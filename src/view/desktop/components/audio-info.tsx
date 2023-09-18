d27b3ec87a959b85adcb60189a8e20f021dd5d4b
formyauto75@gmail.com <formyauto75@gmail.com>
2023-09-18 12:29:46 +0400

    fix: image hide with css

--- src/view/desktop/components/audio-info.tsx
+++ src/view/desktop/components/audio-info.tsx
@@ -6,13 +6,11 @@ export const Image = () => {
 
   return (
     <div className="audio-info">
-      <div className="image-wrapper">
-        <img
-          src={audio.image as string}
-          draggable={false}
-          className={audio.image ? '' : 'hidden'}
-        />
-      </div>
+      {audio.id && (
+        <div className="image-wrapper">
+          <img src={audio.image as string} draggable={false} />
+        </div>
+      )}
       <summary>
         <p className="audio-title">{audio.title}</p>
         <p className="audio-artist">{audio.uploader?.name}</p>
