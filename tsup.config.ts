99b876845e1e8494b0956afdbc64e33b8e984035
formyauto75@gmail.com <formyauto75@gmail.com>
2023-09-23 18:20:17 +0400

    test: css loader

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'tsup';
+import cssModulesPlugin from 'esbuild-css-modules-plugin';
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -10,4 +11,5 @@ export default defineConfig({
   splitting: false,
   format: ['cjs', 'esm'],
   external: ['react'],
+  esbuildPlugins: [cssModulesPlugin()],
 });
